816aac6b91e7d03738f6d1a2bfa4c684cc57fab0
jigasb <sardorbaxtiyorov2134@gmail.com>
2025-06-28 17:44:31 +0500

    Fix WebSocketConfig class and improve broker configuration
    Renamed `WepSocketConfig` to `WebSocketConfig` to correct a typo. Refactored imports and updated `configureMessageBroker` to use a cleaner reference to `MessageBrokerRegistry`.

--- src/main/java/com/example/pdp_project/config/WebSocketConfig.java
+++ src/main/java/com/example/pdp_project/config/WebSocketConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WepSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
@@ -16,9 +17,8 @@ public class WepSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry config) {
+    public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 }
-
