08a4746504fc62a34ad197d23868274a9a315169
Abduvaliy <inosukebest1@example.com>
2025-07-13 15:02:25 +0500

    TripController & TripService fixed

--- src/main/java/com/example/pdp_project/controller/TripController.java
+++ src/main/java/com/example/pdp_project/controller/TripController.java
@@ -1,23 +1,30 @@
 package com.example.pdp_project.controller;
 
+import com.example.pdp_project.entity.Trip;
 import com.example.pdp_project.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/trips") @RequiredArgsConstructor
 public class TripController {
     private final TripService service;
     @GetMapping("/popular")
-    public ResponseEntity<?> popular(){
-        return ResponseEntity.ok(service.popularTrips());
+    public ResponseEntity<?> popular(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(service.popularTrips(page, size));
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> byId(@PathVariable Long id){
-        return ResponseEntity.ok(service.getOneTrip(id));
+        Optional<Trip> oneTrip = service.getOneTrip(id);
+        if(oneTrip.isPresent()){
+            return ResponseEntity.ok(oneTrip.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/example/pdp_project/service/TripService.java
+++ src/main/java/com/example/pdp_project/service/TripService.java
@@ -3,16 +3,25 @@ package com.example.pdp_project.service;
 import com.example.pdp_project.entity.Trip;
 import com.example.pdp_project.repo.TripRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TripService {
     private final TripRepository repo;
-    public List<Trip> popularTrips() {
-        return repo.findTop8ByOrderByRatingDesc();
+
+    public Page<Trip> popularTrips(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "rating"));
+        return repo.findAll(pageable);
     }
-    public Trip getOneTrip(Long id){return repo.findById(id).orElseThrow();}
+
+    public Optional<Trip> getOneTrip(Long id) {
+        return repo.findById(id);
+    }
 }
